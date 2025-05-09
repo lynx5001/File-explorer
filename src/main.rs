8f1d763c77d25b1ad7f8b77672b41ef8ae6057e4
Louis Carlos Roth <info.lcr@protonmail.com>
2025-05-09 15:19:39 +0200

    Implement basic file listing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
+// include filesystem manipulation operations
+use std::fs;
+use std::io;
+//use std::path::Path;
+
+fn main() -> io::Result<()> {
     println!("Hello, File Explorer!");
+
+    // set current dir
+    let current_dir = "."; // Current directory
+
+    // Read the directory
+    let entries = fs::read_dir(current_dir)?;
+
+    // Iterate through the entries and print their names
+    println!("Files and directories in '{}':", current_dir);
+    for entry in entries {
+        let entry = entry?;
+        let path = entry.path();
+        if path.is_dir() {
+            println!("[DIR] {}", path.display());
+        } else {
+            println!("[FILE] {}", path.display());
+        }
+    }
+
+    Ok(())
 }
